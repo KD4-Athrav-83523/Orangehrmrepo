0bda87338d31a4fb9862ac008f1f9216798245f6
unknown <apailwan53@gmail.com>
2026-08-18 17:03:57 +0530

    commiting the internethook up test case code

---
+++ tests/intenethookup.spec.js
@@ -0,0 +1,19 @@
+const { test, expect } = require('@playwright/test');
+
+test('Herokuapp Form Authentication Test', async ({ page }) => {
+  test.setTimeout(60000);
+
+  await page.goto('https://the-internet.herokuapp.com/login', {
+    waitUntil: 'domcontentloaded',
+  });
+
+  await page.locator('#username').fill('tomsmith');
+  await page.locator('#password').fill('SuperSecretPassword!');
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  await expect(page).toHaveURL(/.*secure/);
+  await expect(page.locator('#flash')).toContainText('You logged into a secure area!');
+
+  await page.getByRole('link', { name: 'Logout' }).click();
+  await expect(page).toHaveURL(/.*login/);
+});
